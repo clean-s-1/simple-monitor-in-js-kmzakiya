9d09a35b25a35d26510f48f1558af5e48c3b125c
kmzakiya <zakyakm@gmail.com>
2022-06-16 21:02:25 +0530

    Revert "code change as per review comment"
    This reverts commit 966143e6f9e2d207fa3383ede2751619710c4b3b.

--- bms-monitor.js
+++ bms-monitor.js
@@ -10,18 +10,18 @@ function getDeviationFromMax(max, value) {
   return value > max ? value - max : 0;
 }
 
-function printBreach(breach, state, property, battery) {
+function printBreach(deviationFromMin, deviationFromMax, name, battery) {
+  const breach = deviationFromMin || deviationFromMax;
   console.log(
-    `${property} is ${state} by ${breach} - ${battery.toString()}`
+    `${name} is ${deviationFromMin ? "LOW" : "HIGH"} by ${breach} - ${battery.toString()}`
   );
 }
 
-function checkIfPropertyInRange(key, battery, config, printFunction) {
+function checkIfPropertyInRange(key, battery, config) {
   const deviationFromMin = getDeviationFromMin(config.min, battery[key]);
   const deviationFromMax = getDeviationFromMax(config.max, battery[key]);
-  const breach = deviationFromMin || deviationFromMax;
-  if (breach) {
-    printFunction(breach, deviationFromMin ? 'LOW' : 'HIGH', config.name, battery);
+  if (deviationFromMin || deviationFromMax) {
+    printBreach(deviationFromMin, deviationFromMax, config.name, battery);
     return false;
   }
   return true;
@@ -29,9 +29,9 @@ function checkIfPropertyInRange(key, battery, config, printFunction) {
 
 function batteryIsOk(battery, bmsConfig) {
   return (
-    checkIfPropertyInRange("temperature", battery, bmsConfig.temperature, printBreach) &&
-    checkIfPropertyInRange("stateOfCharge", battery, bmsConfig.stateOfCharge, printBreach) &&
-    checkIfPropertyInRange("chargeRate", battery, bmsConfig.chargeRate, printBreach)
+    checkIfPropertyInRange("temperature", battery, bmsConfig.temperature) &&
+    checkIfPropertyInRange("stateOfCharge", battery, bmsConfig.stateOfCharge) &&
+    checkIfPropertyInRange("chargeRate", battery, bmsConfig.chargeRate)
   );
 }
 
