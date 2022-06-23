6ab110ee57a9d39257e98c1d812a9f616ba37f7c
kmzakiya <zakyakm@gmail.com>
2022-06-23 17:58:45 +0530

    added features for language support and warning

--- bms-monitor.js
+++ bms-monitor.js
@@ -1,50 +1,100 @@
 const { expect } = require("chai");
 const Battery = require("./battery");
-const BMS_CONFIG = require("./bms.config");
+const { BMS_CONFIG, MESSAGES, LEVELS } = require("./bms.config");
 
-function getDeviationFromMin(min, value) {
-  return value < min ? min - value : 0;
-}
-
-function getDeviationFromMax(max, value) {
-  return value > max ? value - max : 0;
-}
-
-function printBreach(deviationFromMin, deviationFromMax, name, battery) {
-  const breach = deviationFromMin || deviationFromMax;
-  console.log(
-    `${name} is ${deviationFromMin ? "LOW" : "HIGH"} by ${breach} - ${battery.toString()}`
-  );
-}
-
-function checkIfPropertyInRange(key, battery, config) {
-  const deviationFromMin = getDeviationFromMin(config.min, battery[key]);
-  const deviationFromMax = getDeviationFromMax(config.max, battery[key]);
-  if (deviationFromMin || deviationFromMax) {
-    printBreach(deviationFromMin, deviationFromMax, config.name, battery);
-    return false;
+const getLevel = (value, min, max) => {
+  if (value < min) {
+    return LEVELS.LOW_BREACH;
+  } else if (value < max) {
+    return LEVELS.NORMAL;
   }
-  return true;
-}
+  return LEVELS.HIGH_BREACH;
+};
 
-function batteryIsOk(battery, bmsConfig) {
-  return (
-    checkIfPropertyInRange("temperature", battery, bmsConfig.temperature) &&
-    checkIfPropertyInRange("stateOfCharge", battery, bmsConfig.stateOfCharge) &&
-    checkIfPropertyInRange("chargeRate", battery, bmsConfig.chargeRate)
-  );
-}
+const isNormal = (value) => {
+  return value === LEVELS.NORMAL;
+};
 
-expect(batteryIsOk(new Battery(0, 70, 0.7), BMS_CONFIG)).to.be.true;
-expect(batteryIsOk(new Battery(25, 70, 0.7), BMS_CONFIG)).to.be.true;
-expect(batteryIsOk(new Battery(45, 70, 0.7), BMS_CONFIG)).to.be.true;
-expect(batteryIsOk(new Battery(50, 70, 0.7), BMS_CONFIG)).to.be.false;
+const getTolerance = (limit, tolerance) => {
+  return (limit * tolerance) / 100;
+};
 
-expect(batteryIsOk(new Battery(40, 10, 0), BMS_CONFIG)).to.be.false;
-expect(batteryIsOk(new Battery(40, 20, 0), BMS_CONFIG)).to.be.true;
-expect(batteryIsOk(new Battery(40, 50, 0), BMS_CONFIG)).to.be.true;
-expect(batteryIsOk(new Battery(40, 80, 0), BMS_CONFIG)).to.be.true;
-expect(batteryIsOk(new Battery(50, 99, 0), BMS_CONFIG)).to.be.false;
+const checkWarningLevel = (value, minTolerance, maxTolerance) => {
+  if (value <= minTolerance) {
+    return LEVELS.LOW_WARNING;
+  } else if (value >= maxTolerance) {
+    return LEVELS.HIGH_WARNING;
+  }
+  return LEVELS.NORMAL;
+};
 
-expect(batteryIsOk(new Battery(25, 50, 0.8), BMS_CONFIG)).to.be.true;
-expect(batteryIsOk(new Battery(40, 80, 0.9), BMS_CONFIG)).to.be.false;
+const print = (messagekey, language) => {
+  console.log(MESSAGES[language][messagekey]);
+};
+
+const getPropertyStatus = (value, config) => {
+  const tolerance = getTolerance(config.max, config.tolerance);
+  let level = getLevel(value, config.min, config.max);
+  const isInRange = isNormal(level);
+  if (isInRange) {
+    level = checkWarningLevel(
+      value,
+      config.min + tolerance,
+      config.max - tolerance
+    );
+  }
+  return { isInRange, level };
+};
+
+const printStatusMessage = (level, config, language) => {
+  if (!isNormal(level)) {
+    const messagekey = config[level];
+    print(messagekey, language);
+  }
+};
+
+const batteryIsOk = ({ stateOfCharge, chargeRate, temperature }, config, language = "en") => {
+  const socStatus = getPropertyStatus(stateOfCharge, config.stateOfCharge);
+  const crStatus = getPropertyStatus(chargeRate, config.chargeRate);
+  const tempStatus = getPropertyStatus(temperature, config.temperature);
+
+  printStatusMessage(socStatus.level, config.stateOfCharge, language);
+  printStatusMessage(crStatus.level, config.chargeRate, language);
+  printStatusMessage(tempStatus.level, config.temperature, language);
+
+  return socStatus.isInRange && crStatus.isInRange && tempStatus.isInRange;
+};
+
+
+expect(getPropertyStatus(30, BMS_CONFIG.stateOfCharge)).to.include({
+  level: LEVELS.NORMAL,
+  isInRange: true,
+});
+expect(getPropertyStatus(22, BMS_CONFIG.stateOfCharge)).to.include({
+  level: LEVELS.LOW_WARNING,
+  isInRange: true,
+});
+expect(getPropertyStatus(50, BMS_CONFIG.temperature)).to.include({
+  level: LEVELS.HIGH_BREACH,
+  isInRange: false,
+});
+
+expect(getPropertyStatus(0.7, BMS_CONFIG.chargeRate)).to.include({
+  level: LEVELS.NORMAL,
+  isInRange: true,
+});
+expect(getPropertyStatus(0.9, BMS_CONFIG.chargeRate)).to.include({
+  level: LEVELS.HIGH_BREACH,
+  isInRange: false,
+});
+
+expect(getPropertyStatus(10, BMS_CONFIG.stateOfCharge)).to.include({
+  level: LEVELS.LOW_BREACH,
+  isInRange: false,
+});
+
+expect(batteryIsOk(new Battery(40, 30, 0.7), BMS_CONFIG)).to.be.true;
+expect(batteryIsOk(new Battery(1, 30, 0.7), BMS_CONFIG)).to.be.true;
+expect(batteryIsOk(new Battery(55, 22, 0.7), BMS_CONFIG)).to.be.false;
+expect(batteryIsOk(new Battery(40, 100, 0.7), BMS_CONFIG)).to.be.false;
+expect(batteryIsOk(new Battery(40, 10, 0.9), BMS_CONFIG)).to.be.false;
